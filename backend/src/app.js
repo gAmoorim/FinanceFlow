7a9134f99796b146cfb3d0c1312972c8c5787f57
gamoorim <gabrielrock012@gmail.com>
2026-03-02 21:20:25 -0300

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,8 +6,8 @@ require('dotenv').config()
 const app = express()
 
 app.use(cors({
-  origin: ['http://localhost:3000', process.env.FRONTEND_URL],
-  credentials: true
+  origin: '*',
+  credentials: false
 }))
 
 app.use(express.json())
